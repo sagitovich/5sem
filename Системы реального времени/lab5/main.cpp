047d82ff2c40c70bac3722f395a73196d6e890e5
Alexandr Sagitov <115216797+sagitovich@users.noreply.github.com>
2024-11-02 17:07:55 +0200

    Add files via upload

---
+++ Системы реального времени/lab5/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+int coins = 10;            // общее количество монет
+bool flag = false;
+int Bob_coins = 0;         // монеты Боба
+int Tom_coins = 0;         // монеты Тома
+std::mutex mtx;            // мьютекс для синхронизации доступа к монетам
+
+void coin_sharing(const std::string& name, int& thief_coins, const int& companion_coins) {
+    while (true) {
+        mtx.lock(); 
+
+        if (coins == 1 && Bob_coins == Tom_coins) {
+            coins--;
+            std::cout << "Последняя монета достается покойнику. Осталось монет: " << coins << std::endl;
+            mtx.unlock();
+            break;
+        }
+
+        if (coins > 0 && thief_coins <= companion_coins) {
+            thief_coins++;
+            coins--;
+            std::cout << name << " взял монету. У него теперь "
+                      << thief_coins << " монет. Осталось монет: "
+                      << coins << std::endl;
+            mtx.unlock();
+        } else {
+            mtx.unlock();
+            if (coins == 0) {
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    if (coins % 2 != 0) {
+        flag = true;
+    }
+
+    std::thread bob_thread(coin_sharing, "Боб", std::ref(Bob_coins), std::ref(Tom_coins));
+    std::thread tom_thread(coin_sharing, "Том", std::ref(Tom_coins), std::ref(Bob_coins));
+
+    bob_thread.join();
+    tom_thread.join();
+
+    std::cout << "Итог:\n"
+              << "Монет у Боба: " << Bob_coins << "\n"
+              << "Монет у Тома: " << Tom_coins << "\n"
+              << "Всего монет у воров: " << (Bob_coins + Tom_coins) << "\n";
+    if (flag) {
+        std::cout << "Покойник получил последнюю монету." << std::endl;
+    }
+
+    return 0;
+}
